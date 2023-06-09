d3a434f39418f1a371ab45e25a980d1bcf9ef0ae
Simeon Rangelov <67736865+Mr-Sims@users.noreply.github.com>
2023-06-09 15:21:21 +0300

    refactored PostForm using useRef

--- src/components/NewPost/PostForm.js
+++ src/components/NewPost/PostForm.js
@@ -1,115 +1,62 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import classes from "./PostForm.module.css";
+import Button from "../UI/Button";
 
 
 const PostForm = ({ onCancel, onAddPost }) => {
+    const systolicInputRef = useRef('');
+    const diastolicInputRef = useRef('');
+    const pulseInputRef = useRef('');
+    const dateInputRef = useRef('');
+    const timeInputRef = useRef('');
+    const feelInputRef = useRef('')
 
-    const [systolic, setSystolic] = useState('');
     const [systolicIsValid, setSystolicIsvalid] = useState(true);
-
-    const [diastolic, setDiastolic] = useState('');
     const [diastolicIsValid, setDiastolicIsValid] = useState(true);
-
-    const [pulse, setPulse] = useState('');
     const [pulseIsValid, setPulseIsValid] = useState(true);
-
-    const [date, setDate] = useState('');
     const [dateIsValid, setDateIsValid] = useState(true);
-
-    const [time, setTime] = useState('');
     const [timeIsValid, setTimeIsValid] = useState(true);
-
-    const [feel, setFeel] = useState('');
     const [feelIsValid, setFeelIsValid] = useState(true);
 
     const validations = {
-        systolic: [systolic, setSystolicIsvalid],
-        diastolic: [diastolic, setDiastolicIsValid],
-        pulse: [pulse, setPulseIsValid],
-        date: [date, setDateIsValid],
-        time: [time, setTimeIsValid],
-        feel: [feel, setFeelIsValid]
+        systolic: [systolicInputRef.current.value || '', setSystolicIsvalid],
+        diastolic: [diastolicInputRef.current.value || '', setDiastolicIsValid],
+        pulse: [pulseInputRef.current.value || '', setPulseIsValid],
+        date: [dateInputRef.current.value || '', setDateIsValid],
+        time: [timeInputRef.current.value || '', setTimeIsValid],
+        feel: [feelInputRef.current.value || '', setFeelIsValid]
     }
-
 
     // SYSTOLIC controls
-    const systolicInputChangeHandler = (e) => {
-        setSystolic(e.target.value);
-        if (e.target.value.trim().length !== 0) {
-            setSystolicIsvalid(true);
-        }
-    }
-    const systolicBlurChangeHandler = () => {
-        if (systolic.trim().length === 0) {
-            setSystolicIsvalid(false);
-        } 
+    const systolicBlurChangeHandler = (e) => {
+        e.target.value.trim().length === 0 ? setSystolicIsvalid(false) : setSystolicIsvalid(true);
     }
 
     //DIASTOLIC controls
-    const diastolicInputChangeHandler = (e) => {
-        setDiastolic(e.target.value);
-        if (e.target.value.trim().lenght !== 0) {
-            setDiastolicIsValid(true);
-        }
-    }
     const diastolicBlurChangeHandler = (e) => {
-        if (diastolic.trim().length === 0) {
-            setDiastolicIsValid(false);
-        } 
+        e.target.value.trim().length === 0 ? setDiastolicIsValid(false) : setDiastolicIsValid(true)
     }
-    
+
     // PULSE controls
-    const pulseInputChangeHandler = (e) => {
-        setPulse(e.target.value);
-        if (e.target.value.trim().lenght !== 0) {
-            setPulseIsValid(true);
-        }
-    }
     const pulseBlurChangeHandler = (e) => {
-        if (pulse.trim().length === 0) {
-            setPulseIsValid(false);
-        } 
+        e.target.value.trim().lenght === 0 ? setPulseIsValid(false) : setPulseIsValid(true);
     }
 
     //DATE controls
-    const dateInputChangeHandler = (e) => {
-        setDate(e.target.value);
-        if (e.target.value.trim().lenght !== 0) {
-            setDateIsValid(true);
-        }
-    }
     const dateBlurChangeHandler = (e) => {
-        if (date.trim().length === 0) {
-            setDateIsValid(false);
-        } 
+        e.target.value.trim().lenght === 0 ? setDateIsValid(false) : setDateIsValid(true);
     }
 
     // TIME controls
-    const timeInputChangeHandler = (e) => {
-        setTime(e.target.value);
-        if (e.target.value.trim().lenght !== 0) {
-            setTimeIsValid(true);
-        }
-    }
     const timeBlurChangeHandler = (e) => {
-        if (time.trim().length === 0) {
-            setTimeIsValid(false);
-        } 
+        e.target.value.trim().lenght === 0 ? setTimeIsValid(false) : setTimeIsValid(true);
     }
 
     //FEEL controls
-    const feelInputChangeHandler = (e) => {
-        setFeel(e.target.value);
-        if (e.target.value.trim().lenght !== 0) {
-            setFeelIsValid(true);
-        }
-    }
     const feelBlurChangeHandler = (e) => {
-        if (feel.trim().length === 0) {
-            setFeelIsValid(false);
-        } 
-    }
+        e.target.value.trim().lenght === 0 ? setFeelIsValid(false) : setFeelIsValid(true);
 
+    }
 
     const submitHandler = (e) => {
         e.preventDefault();
@@ -120,22 +67,23 @@ const PostForm = ({ onCancel, onAddPost }) => {
             }
         }
 
-        if (systolic.trim().length === 0 ||
-            diastolic.trim().length === 0 ||
-            pulse.trim().length === 0 ||
-            date.trim().length === 0 ||
-            time.trim().length === 0 ||
-            feel.trim().length === 0
+        if (systolicInputRef.current.value.trim().length === 0 ||
+            diastolicInputRef.current.value.trim().length === 0 ||
+            pulseInputRef.current.value.trim().length === 0 ||
+            dateInputRef.current.value.trim().length === 0 ||
+            timeInputRef.current.value.trim().length === 0 ||
+            feelInputRef.current.value.trim().length === 0
         ) {
             return;
         }
 
         onAddPost({
-            systolic,
-            diastolic,
-            pulse,
-            date: new Date(date),
-            time: time.split(' '),
+            systolic: +systolicInputRef.current.value,
+            diastolic: +diastolicInputRef.current.value,
+            pulse: +pulseInputRef.current.value,
+            date: new Date(dateInputRef.current.value),
+            time: timeInputRef.current.value.toString(),
+            feel: feelInputRef.current.value
         })
     }
 
@@ -151,9 +99,8 @@ const PostForm = ({ onCancel, onAddPost }) => {
                         min="60"
                         max="200"
                         placeholder="Write your systolic measurement"
-                        value={systolic}
-                        onChange={systolicInputChangeHandler}
                         onBlur={systolicBlurChangeHandler}
+                        ref={systolicInputRef}
                     />
                 </div>
                 <div className={`${classes.control} ${!diastolicIsValid && classes.invalid}`}>
@@ -165,10 +112,9 @@ const PostForm = ({ onCancel, onAddPost }) => {
                         min="40"
                         max="120"
                         placeholder="Write your diastolic measurement"
-                        value={diastolic}
-                        onChange={diastolicInputChangeHandler}
                         onBlur={diastolicBlurChangeHandler}
-/>
+                        ref={diastolicInputRef}
+                    />
                 </div>
                 <div className={`${classes.control} ${!pulseIsValid && classes.invalid}`}>
                     <label htmlFor="pulse">Pulse</label>
@@ -179,9 +125,8 @@ const PostForm = ({ onCancel, onAddPost }) => {
                         min="30"
                         max="200"
                         placeholder="Write your pulse measurement"
-                        value={pulse}
-                        onChange={pulseInputChangeHandler}
                         onBlur={pulseBlurChangeHandler}
+                        ref={pulseInputRef}
                     />
                 </div>
                 <div>
@@ -192,9 +137,8 @@ const PostForm = ({ onCancel, onAddPost }) => {
                             type="date"
                             id="date"
                             name="date"
-                            value={date}
-                            onChange={dateInputChangeHandler}
                             onBlur={dateBlurChangeHandler}
+                            ref={dateInputRef}
                         />
                     </div>
                     <div className={`${classes.control} ${!timeIsValid && classes.invalid}`}>
@@ -203,9 +147,8 @@ const PostForm = ({ onCancel, onAddPost }) => {
                             type="time"
                             id="time"
                             name="time"
-                            value={time}
-                            onChange={timeInputChangeHandler}
                             onBlur={timeBlurChangeHandler}
+                            ref={timeInputRef}
                         />
                     </div>
                 </div>
@@ -216,15 +159,14 @@ const PostForm = ({ onCancel, onAddPost }) => {
                         id="feel"
                         cols="30"
                         rows="3"
-                        value={feel}
-                        onChange={feelInputChangeHandler}
                         onBlur={feelBlurChangeHandler}
                         placeholder="Please give a short recap of how you feel!"
+                        ref={feelInputRef}
                     ></textarea>
                 </div>
                 <div className='actions'>
-                    <button type='button' onClick={onCancel} >Cancel</button>
-                    <button type="submit">Submit</button>
+                    <Button type='button' onClick={onCancel} >Cancel</Button>
+                    <Button type="submit">Submit</Button>
                 </div>
             </div>
         </form>
